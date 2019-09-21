cb5060a63c22c3b06d8b23e75a93afc86a781f8f
pedroth <pedrotiago92@gmail.com>
2019-09-21 15:25:01 +0100

    Import visualExperiment jsLib 2 here

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/Nabla/nabla.js",
+  output: {
+    path: path.resolve("./"),
+    filename: "dist/index.js",
+    library: "Nabla",
+    // Expose the default export.
+    libraryExport: "default"
+  }
+};
