2bb02f70892fc56fce68e24c6d5980e6b40ba012
pedroth <pedrotiago92@gmail.com>
2019-10-27 14:55:01 +0000

    changed webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: "./src/Nabla/nabla.js",
   output: {
     path: path.resolve("./"),
-    filename: "dist/index.js",
+    filename: "index.js",
     library: "Nabla",
     // Expose the default export.
     libraryExport: "default"
