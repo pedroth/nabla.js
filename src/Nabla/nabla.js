da97f6a16b1a728c40a5c0bb08e767a4cd888141
cristovao <pedro.cristovao@mov.ai>
2020-05-17 10:29:03 +0100

    update lib

--- src/Nabla/nabla.js
+++ src/Nabla/nabla.js
@@ -4,14 +4,16 @@ import ImageIO from "../Canvas/main/ImageIO.js";
 import Stream from "../Stream/main/Stream.js";
 import ArrayUtils from "../ArrayUtils/main/ArrayUtils";
 import Sort from "../Sort/main/Sort";
+import EditDistance from "../EditDistance/main/EditDistance";
 
-const Nabla = {};
-
-Nabla.Canvas = Canvas;
-Nabla.Canvas2D = Canvas2D;
-Nabla.ImageIO = ImageIO;
-Nabla.Stream = Stream;
-Nabla.ArrayUtils = ArrayUtils;
-Nabla.Sort = Sort;
+const Nabla = {
+  Canvas,
+  Canvas2D,
+  ImageIO,
+  Stream,
+  ArrayUtils,
+  Sort,
+  EditDistance
+};
 
 export default Nabla;
